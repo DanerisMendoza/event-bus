18f2e21be836639bcd0c613237fad0bdcd13fb5e
Daneris <danerismendoza096@gmail.com>
2023-06-20 17:28:27 +0800

    initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <HelloWorld/>
+  <SecondPage/>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import SecondPage from './components/SecondPage.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HelloWorld,SecondPage
   }
 }
 </script>
---
+++ src/components/SecondPage.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+      <h1> Second Page </h1>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'SecondPage',
+    mounted() { 
+    this.emitter.on("toggle-sidebar", () => {
+      console.log('triggered');
+    });
+  }
+
+  }
+  </script>
+  
+  
